624e0baf31598c6bd958451c0911f4e48066d5ab
Mark Vu <markvu2003@gmail.com>
2026-02-03 22:55:28 -0800

    feat: added remote state file terraform

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,23 +1,7 @@
-# Remote state storage in Google Cloud Storage
-#
-# Setup (one-time, run manually):
-# 
-# 1. Create bucket:
-#    gcloud storage buckets create gs://PROJECT-ID-tf-state \
-#      --location=us-west1 \
-#      --uniform-bucket-level-access
-#
-# 2. Enable versioning:
-#    gcloud storage buckets update gs://PROJECT-ID-tf-state --versioning
-#
-# 3. Uncomment the backend block below and replace PROJECT-ID
-#
-# 4. Run: terraform init -migrate-state
-
-# terraform {
-#   backend "gcs" {
-#     bucket = "PROJECT-ID-tf-state"
-#     prefix = "artifact-registry"
-
-#   }
-# }
+# Remote state file in Google Cloud Storage
+terraform {
+  backend "gcs" {
+    bucket = "${var.project_id}-tf-state"
+    prefix = "artifact-registry"
+  }
+}
