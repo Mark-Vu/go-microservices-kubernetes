8d7e3eac49e800f850efd5e01e9cea37f020d441
Mark Vu <markvu2003@gmail.com>
2026-02-03 22:59:41 -0800

    fix(actions): inject secrets in terraform

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,8 @@
 # Remote state file in Google Cloud Storage
+# Bucket name is configured via -backend-config or environment variable
 terraform {
   backend "gcs" {
-    bucket = "${var.project_id}-tf-state"
+    # bucket = "" is configured via CI workflow
     prefix = "artifact-registry"
   }
 }
