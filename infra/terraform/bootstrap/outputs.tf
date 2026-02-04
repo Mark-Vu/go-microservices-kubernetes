3cbb0cd8502c4880e65cc901837e61023f3b1d3e
Mark Vu <markvu2003@gmail.com>
2026-02-03 18:25:14 -0800

    chore(terraform): run terraform bootstrap, bucket created confirmed

--- infra/terraform/bootstrap/outputs.tf
+++ infra/terraform/bootstrap/outputs.tf
@@ -1,18 +1,18 @@
 output "state_bucket_name" {
   description = "Name of the Terraform state bucket (use this in backend.tf)"
-  value       = google_storage_bucket.terraform_state.name
+  value       = google_storage_bucket.tf_state.name
 }
 
 output "next_steps" {
   description = "What to do next"
   value       = <<-EOT
-    State bucket created: ${google_storage_bucket.terraform_state.name}
+    State bucket created: ${google_storage_bucket.tf_state.name}
     
     Next steps:
-    1. cd ..
+    1. cd ../terraform
     2. Edit backend.tf - uncomment backend block
-    3. Update bucket name to: ${google_storage_bucket.terraform_state.name}
+    3. Update bucket name to: ${google_storage_bucket.tf_state.name}
     4. Run: terraform init
-    5. Main config will now use remote state
+    5. Your main config will now use remote state
   EOT
 }
